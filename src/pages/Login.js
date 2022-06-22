d8a1e8becd5dafaac4b38bfbc4e5c5e0eafff7dd
Samuelr499 <samuelribeiro2@hotmail.com>
2022-06-22 18:40:44 -0300

    requisito 1 Login e setup redux

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,79 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { addEmail } from '../actions/index';
 
 class Login extends React.Component {
-  render() {
-    return <div>Login</div>;
-  }
+   state = {
+     email: '',
+     senha: '',
+     buttonDisable: true,
+   };
+
+   // https://pt.stackoverflow.com/questions/1386/express%C3%A3o-regular-para-valida%C3%A7%C3%A3o-de-e-mail me ajudou a interpretar regex
+   handleOnChange = (event) => {
+     const { name, value } = event.target;
+     const regex = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i;
+     this.setState({ [name]: value });
+
+     this.setState(({ senha, email }) => {
+       const minLength = 6;
+       if (regex.test(email) && senha.length >= minLength) {
+         return this.setState({ buttonDisable: false });
+       }
+       return this.setState({ buttonDisable: true });
+     });
+   };
+
+onbuttonClick = () => {
+  const { history, dispatch } = this.props;
+  const { email } = this.state;
+  dispatch(addEmail(email));
+  history.push('/carteira');
 }
 
-export default Login;
+render() {
+  const { email, senha, buttonDisable } = this.state;
+  return (
+    <div className="login">
+      <label htmlFor="email">
+        e-mail
+        <input
+          type="email"
+          name="email"
+          required
+          data-testid="email-input"
+          id="email"
+          value={ email }
+          onChange={ this.handleOnChange }
+        />
+      </label>
+      <label htmlFor="senha">
+        senha (de no minimo 6 caracter)
+        <input
+          type="password"
+          data-testid="password-input"
+          name="senha"
+          id="senha"
+          value={ senha }
+          onChange={ this.handleOnChange }
+          minLength="6"
+          required
+        />
+      </label>
+      <button
+        type="button"
+        disabled={ buttonDisable }
+        onClick={ this.onbuttonClick }
+      >
+        Entrar
+      </button>
+    </div>
+  );
+}
+}
+export default connect()(Login);
+Login.propTypes = {
+  history: PropTypes.string.isRequired,
+  dispatch: PropTypes.func.isRequired,
+};
